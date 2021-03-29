42c098a4374ccf2d2c483fc4fd60c25e9b893e48
Stadnik Andrii <stadnikandriy1@gmail.com>
2021-03-29 05:31:59 +0300

    Add dockerfile, change main

--- main.cc
+++ main.cc
@@ -11,7 +11,7 @@ using namespace std;
 std::string parseArgs(int argc, const char *argv[]) {
   if (argc != 2) {
     std::cout << "Usage: ./out path" << std::endl;
-    return 0;
+    exit(1);
   }
 
   return argv[1];
@@ -21,37 +21,33 @@ template <typename T>
 std::string type_name();
 
 int main(int argc, const char *argv[]) {
+  string fn = parseArgs(argc, argv);
+  std::cout << argv[1] << std::endl;
   ios_base::sync_with_stdio(false);
 
-  /* string fn = parseArgs(argc, argv); */
-  (void)argc, (void)argv;
-  for (const auto &fn :
-       {"inputs/elim_while.txt", "inputs/out.txt", "inputs/lift.txt",
-        "inputs/indirectly_related.txt", "inputs/working_complicated.txt"}) {
-    list<string> s = algs::io::readFileToList(fn);
-    /* for (const auto &l : s) std::cout << l << std::endl; */
+  list<string> s = algs::io::readFileToList(fn);
+  /* for (const auto &l : s) std::cout << l << std::endl; */
 
-    ast::Stmt::ptr st = ast::StatementFactory::gen_statement(s);
-    /* std::cout << *st << std::endl; */
-    /* dbg("POTATO"); */
+  ast::Stmt::ptr st = ast::StatementFactory::gen_statement(s);
+  /* std::cout << *st << std::endl; */
+  /* dbg("POTATO"); */
 
-    /* st->dump("/tmp/original.cc"); */
+  st->dump("/tmp/original.cc");
 
-    st = goto_elimination::eliminateGoto(move(st), ostream(0));
+  st = goto_elimination::eliminateGoto(move(st), ostream(0));
 
-    /*   st->dump("/tmp/changed.cc"); */
+  st->dump("/tmp/changed.cc");
 
-    /*   std::cout << *st << std::endl; */
+  std::cout << *st << std::endl;
 
-    /*   system("g++ /tmp/original.cc -o /tmp/original"); */
-    /*   std::cout << "Original output:" << std::endl; */
-    /*   system("/tmp/original"); */
+  system("g++ /tmp/original.cc -o /tmp/original");
+  std::cout << "Original output:" << std::endl;
+  system("/tmp/original");
 
-    /*   std::cout << *st << std::endl; */
-    /*   system("g++ /tmp/changed.cc -o /tmp/changed"); */
-    /*   std::cout << "Changed output:" << std::endl; */
-    /*   system("/tmp/changed"); */
-  }
+  std::cout << *st << std::endl;
+  system("g++ /tmp/changed.cc -o /tmp/changed");
+  std::cout << "Changed output:" << std::endl;
+  system("/tmp/changed");
 
   return 0;
 }
